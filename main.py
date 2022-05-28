858878707eb00c593b91e15cbb8a5ae8945e4277
Kolmakov VI <kolmakowladimir@gmail.com>
2022-05-28 23:09:25 +0300

    add test.py for testing. Add new def for send package nubers checking

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
+import json
+
+import requests
+
+
 def read_api_key():
     with open("C:/pyproj/api_key_pilot.txt", "r") as file:
         for line in file:
@@ -5,13 +10,26 @@ def read_api_key():
     return api_key
 
 
-def ping_sms(api_key):
-    pass
+def ping_sms_single(api_key):
+    # url_balance = "https://smspilot.ru/api.php?balance=rur&apikey=" + api_key  # ссылка на запрос баланса кошелька
+    url_ping = "https://smspilot.ru/api.php?send=HLRVIP&to=79087964781&callback=https%3A%2F%2Fexample.com%2Fhlr-status.php&apikey=XYZ&format=v"
+    # пример ссылки на запрос HLRVIP
+    # https://smspilot.ru/api.php?send=HLRVIP&to=79087964781&callback=https%3A%2F%2Fexample.com%2Fhlr-status.php&apikey=XYZ&format=v
+    # print(url)
+    r = requests.get(url_ping)
+    j = json.loads(r.text)
+    print(j)
 
+
+def ping_sms_paсkage(api_key, nubmers):
+    url = "https://smspilot.ru/api2.php"
+    param = {'api_key': api_key, "text": "HLRVIP"}
+    print(param['text'])
 
 
 if __name__ == '__main__':
     api_key = read_api_key()
-
-    ping_sms(api_key)
+    numbers = ["79686073656", "79647976061", "79533525333"]
+    # ping_sms_single(api_key) # call def with single nubm checking
+    ping_sms_paсkage(api_key, numbers)
 
